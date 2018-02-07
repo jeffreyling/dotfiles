68df84537cbd8697d6be4125ba501263060ad956
Jeffrey Ling <jeffreyling@i.vicarious>
2018-02-07 13:49:25 -0800

    .

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ cd ~
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir ~/.vim/colors
 mv dotfiles/solarized.vim ~/.vim/colors/
+echo "Moving .vimrc, .bash_custom, .tmux.conf"
+mv dotfiles/.vimrc ~
 mv dotfiles/.bash_custom ~
 mv dotfiles/.tmux.conf ~
 
